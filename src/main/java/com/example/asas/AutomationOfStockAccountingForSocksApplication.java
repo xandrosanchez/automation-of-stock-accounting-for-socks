40ac226ecb3a26355da00cac8d915857a14c7a16
xandrosanchez <din.myrk@gmail.com>
2023-06-21 23:12:42 +0300

    Initial commit

---
+++ src/main/java/com/example/asas/AutomationOfStockAccountingForSocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.asas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationOfStockAccountingForSocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomationOfStockAccountingForSocksApplication.class, args);
+    }
+
+}
