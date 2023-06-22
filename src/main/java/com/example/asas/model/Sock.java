e1500961cb8fa0f2fd0ad5ff147e2b232ecc36bd
xandrosanchez <din.myrk@gmail.com>
2023-06-23 00:10:22 +0300

    Добавлена модель Sock

---
+++ src/main/java/com/example/asas/model/Sock.java
@@ -0,0 +1,40 @@
+package com.example.asas.model;
+
+public class Sock {
+    private String color;
+    private int cottonPart;
+    private int quantity;
+
+    public Sock() {
+    }
+
+    public Sock(String color, int cottonPart, int quantity) {
+        this.color = color;
+        this.cottonPart = cottonPart;
+        this.quantity = quantity;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCottonPart() {
+        return cottonPart;
+    }
+
+    public void setCottonPart(int cottonPart) {
+        this.cottonPart = cottonPart;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
