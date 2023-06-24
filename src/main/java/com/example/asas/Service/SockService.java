03044174a4c16d8e0bc43fb6462352b606564a96
xandrosanchez <din.myrk@gmail.com>
2023-06-25 01:18:39 +0300

    Доработал сервис Доработал репозиторий

--- src/main/java/com/example/asas/Service/SockService.java
+++ src/main/java/com/example/asas/Service/SockService.java
@@ -13,15 +13,40 @@ public class SockService {
     private SockRepository sockRepository;
 
     public Sock incomeSock(String color, int cottonPart, int quantity) {
-        Sock sock = checkingForSocks(color, cottonPart);
+        Sock sock = checkingForSocks(color.toLowerCase(), cottonPart);
         if (sock == null) {
-            sock = new Sock(color, cottonPart, quantity);
+            sock = new Sock(color.toLowerCase(), cottonPart, quantity);
         } else {
             sock.setQuantity(sock.getQuantity() + quantity);
         }
         return sockRepository.save(sock);
     }
 
+    public Sock outcomeSock(String color, int cottonPart, int quantity) {
+        Sock sock = checkingForSocks(color, cottonPart);
+        if (sock == null) {
+            return null;
+        } else {
+            quantity = sock.getQuantity() - quantity;
+            if (quantity <= 0) {
+                sockRepository.delete(sock);
+                sock.setQuantity(0);
+                return sock;
+            } else {
+                sock.setQuantity(quantity);
+                return sockRepository.save(sock);
+            }
+        }
+    }
+
+    public int getTotalQuantity(String color, String operation, int cottonPart) {
+        return switch (operation) {
+            case "moreThan" -> sockRepository.countByColorIgnoreCaseAndCottonPartGreaterThan(color, cottonPart);
+            case "lessThan" -> sockRepository.countByColorIgnoreCaseAndCottonPartLessThan(color, cottonPart);
+            case "equal" -> sockRepository.countByColorIgnoreCaseAndCottonPartEquals(color, cottonPart);
+            default -> throw new IllegalArgumentException("Invalid operation: " + operation);
+        };
+    }
 
     private Sock checkingForSocks(String color, int cottonPart) {
         Collection<Sock> socks = sockRepository.findSockByColorIgnoreCaseAndCottonPart(color, cottonPart);
--- src/main/java/com/example/asas/repository/SockRepository.java
+++ src/main/java/com/example/asas/repository/SockRepository.java
@@ -7,4 +7,7 @@ import java.util.Collection;
 
 public interface SockRepository extends JpaRepository<Sock, Long> {
     Collection<Sock> findSockByColorIgnoreCaseAndCottonPart(String color, int cottonPart);
+    int countByColorIgnoreCaseAndCottonPartGreaterThan(String color, int cottonPart);
+    int countByColorIgnoreCaseAndCottonPartLessThan(String color, int cottonPart);
+    int countByColorIgnoreCaseAndCottonPartEquals(String color, int cottonPart);
 }
