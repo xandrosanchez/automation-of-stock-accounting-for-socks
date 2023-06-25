5d1494b1fb333ce240c34bff8dcf195ea8a22f99
xandrosanchez <din.myrk@gmail.com>
2023-06-25 20:07:24 +0300

    Добавил spring doc

--- src/main/java/com/example/asas/controller/SockController.java
+++ src/main/java/com/example/asas/controller/SockController.java
@@ -2,6 +2,9 @@ package com.example.asas.controller;
 
 import com.example.asas.Service.SockService;
 import com.example.asas.model.Sock;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,7 @@ public class SockController {
     @Autowired
     private SockService sockService;
 
+    @Operation(summary = "Регистрирует поступление носков")
     @PostMapping("/income")
     public ResponseEntity<Void> registerIncome(@Valid @RequestBody Sock sock) {
         try {
@@ -29,6 +33,7 @@ public class SockController {
         }
     }
 
+    @Operation(summary = "Регистрирует списание носков")
     @PostMapping("/outcome")
     public ResponseEntity<Void> registerOutcome(@Valid @RequestBody Sock sock) {
         try {
@@ -41,10 +46,14 @@ public class SockController {
         }
     }
 
+    @Operation(summary = "Получает общее количество носков")
     @GetMapping
     public ResponseEntity<Integer> getTotalSocksCount(
+            @Parameter(in = ParameterIn.QUERY, name = "color", description = "Цвет носков")
             @RequestParam(required = false) String color,
+            @Parameter(in = ParameterIn.QUERY, name = "operation", description = "Операция сравнения количества носков")
             @RequestParam(required = false) String operation,
+            @Parameter(in = ParameterIn.QUERY, name = "cottonPart", description = "Содержание хлопка в носках")
             @RequestParam(required = false) Integer cottonPart
     ) {
         if (color == null || operation == null || cottonPart == null) {
